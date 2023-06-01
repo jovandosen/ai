741c97f1d5a0e470d1fcea53fe7fc3bd2cad2562
Jovan Dosen <jovandosen1994@gmail.com>
2023-06-01 10:47:09 +0200

    Loader updated

--- AI/loader/app.js
+++ AI/loader/app.js
@@ -1,14 +1,14 @@
 const container = document.getElementById("container");
 
 // Create 100 div elements
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 50; i++) {
     const div = document.createElement('div');
     div.className = 'circle';
     container.appendChild(div);
 }
   
 // Calculate the angle between each div
-const angle = (2 * Math.PI) / 100;
+const angle = (2 * Math.PI) / 50;
 
 // Position the divs in a circle
 const circleDivs = document.querySelectorAll('.circle');
@@ -19,3 +19,24 @@ circleDivs.forEach((div, index) => {
     div.style.left = x + 'px';
     div.style.top = y + 'px';
 });
+
+var initial = 0;
+var interval;
+
+function addStyle() {
+
+    circleDivs.forEach((div, index) => {
+        div.classList.remove("circle-background");
+    });
+
+    if(initial >= 50) {
+        initial = 0;
+    }
+
+    circleDivs[initial].classList.add("circle-background");
+    
+    initial++;
+
+}
+
+interval = setInterval(addStyle, 30);
